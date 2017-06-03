251aac9b556681e4b11bb21e650162d0cbb8ca8a
wenqs27 <wenqs27@gmail.com>
2017-06-03 22:05:59 +0800

    add item pipeline.

--- tianyancha_spider/pipelines.py
+++ tianyancha_spider/pipelines.py
@@ -7,5 +7,14 @@
 
 
 class TianyanchaSpiderPipeline(object):
+    def __init__(self):
+        self.item_cache = {}
+
     def process_item(self, item, spider):
+        if item['code']:
+            try:
+                self.item_cache[item['code']] = self.item_cache[item['code']].update(item)
+                return self.item_cache[item['code']]
+            except KeyError:
+                return item
         return item
