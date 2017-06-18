a30bb3f3197fb47962a21f6398c5e1cc7952a1d6
wenqiushi <wenqiushi@meituan.com>
2017-06-18 17:29:58 +0800

    remove comments.

--- tianyancha_spider/items.py
+++ tianyancha_spider/items.py
@@ -1,15 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
 class TianyanchaSpiderItem(scrapy.Item):
-    # define the fields for your item here like:
     status = scrapy.Field()
     name = scrapy.Field()
     code = scrapy.Field()
